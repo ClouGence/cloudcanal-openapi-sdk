3a21c70a6264edf1fd56581e693b475630c5df3a
zylicfc <zylicfc@192.168.0.168>
2021-11-18 09:35:05 +0800

    fix(test):remove sensitive data

--- src/test/java/com/clougence/cloudcanal/openapi/sdk/ConstantApiTest.java
+++ src/test/java/com/clougence/cloudcanal/openapi/sdk/ConstantApiTest.java
@@ -1,13 +1,13 @@
 package com.clougence.cloudcanal.openapi.sdk;
 
-import com.clougence.cloudcanal.openapi.sdk.constant.listspecs.ListSpecsRequest;
-import com.clougence.cloudcanal.openapi.sdk.constant.listspecs.ListSpecsResponse;
-import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.clougence.cloudcanal.openapi.sdk.common.CcClient;
 import com.clougence.cloudcanal.openapi.sdk.common.UserProfile;
+import com.clougence.cloudcanal.openapi.sdk.constant.listspecs.ListSpecsRequest;
+import com.clougence.cloudcanal.openapi.sdk.constant.listspecs.ListSpecsResponse;
+import com.google.gson.Gson;
 
 /**
  * @author bucketli 2021/11/17 17:48:18
@@ -17,7 +17,7 @@ public class ConstantApiTest {
     @Test
     public void testListSpecifications() {
         try {
-            UserProfile profile = new UserProfile("127.0.0.1:8111", "akpv341h03z9e111322l4m233abj0ov6flk27539p5l2dkd807up2t967as4e8f", "skd7h61t62gxh3tv4sd1816nk0610u785u669b3mq0mg92avx1y47512yv15v6e");
+            UserProfile profile = new UserProfile("127.0.0.1:8111", "your ak from cloudcanal", "your sk from cloudcanal");
             CcClient client = profile.genClient();
             ListSpecsRequest request = new ListSpecsRequest();
             request.setDataJobType("SYNC");
